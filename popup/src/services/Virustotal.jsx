a093b2db23f52cbc84dd9c190b67ff7490d5b6dc
Abhijit Ghosh <a7ghosh@gmail.com>
2026-07-04 00:33:46 +0530

    Implement VirusTotal analysis polling

--- popup/src/services/Virustotal.jsx
+++ popup/src/services/Virustotal.jsx
@@ -1,4 +1,7 @@
   const apiKey = import.meta.env.VITE_VIRUSTOTAL_API_KEY;
+  const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+  const attempts =10;
+  const delay = 5000; // 5 seconds
   export const checkURL = async (url) => {
     const options = {
       method: 'POST',
@@ -10,6 +13,32 @@
       body: new URLSearchParams({url})
     };
       const response = await fetch ('https://www.virustotal.com/api/v3/urls', options);
+      if (!response.ok) {
+          throw new Error("Failed to submit URL");
+      }
       const result = await response.json();
-      return result;
+      console.log(result);
+      const analysisId = result.data.id;
+      for (let attempt = 1; attempt <= attempts; attempt++){
+        const analysisResponse = await fetch(`https://www.virustotal.com/api/v3/analyses/${analysisId}`, {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            'x-apikey': apiKey
+          }
+        });
+        if (!analysisResponse.ok) {
+            throw new Error("Failed to retrieve analysis");
+        }
+        const analysisData = await analysisResponse.json();
+        console.log(analysisData.data.attributes.status);
+        if (analysisData.data.attributes.status === 'completed') {
+          console.log("Analysis completed successfully");
+          console.log(analysisData);
+          return analysisData;
+        }
+        console.log(`Attempt ${attempt} failed. Retrying in ${delay / 1000} seconds...`);
+        await sleep(delay);
+        }
+        throw new Error("Analysis did not complete in time - Timed out");
   }
